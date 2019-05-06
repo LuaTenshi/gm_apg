c3f20a9ff2bfeb93bea346926ab975404c8452b3
LuaTenshi <tenshi@mirai.red>
2019-05-06 15:33:56 -0400

    updated tools.lua

--- lua/apg/modules/tools.lua
+++ lua/apg/modules/tools.lua
@@ -20,7 +20,6 @@ function APG.canTool( ply, tool, ent )
 		if ent.CPPICanTool then
 			return ent:CPPICanTool(ply, tool)
 		end -- Let CPPI handle things from here.
-
 	end
 
 	if APG.cfg[ "checkCanTool" ].value and ply.APG_CantPickup == true then-- If we can't pickup we can't tool either.
@@ -45,27 +44,32 @@ end)
 ]]----------------------
 
 APG.hookAdd(mod, "CanTool", "APG_ToolSpamControl", function(ply)
-	if not APG.cfg[ "blockToolSpam" ].value then return end
-	ply.APG_ToolCTRL = ply.APG_ToolCTRL or {}
+	if APG.cfg[ "blockToolSpam" ].value then
+		ply.APG_ToolCTRL = ply.APG_ToolCTRL or {}
 
-	local ply = ply.APG_ToolCTRL
-	ply.curTime = CurTime()
-	ply.toolDelay = ply.toolDelay or 0
-	ply.toolUseTimes = ply.toolUseTimes or 0
+		local ply = ply.APG_ToolCTRL
+		local diff = 0
 
-	if ply.curTime > ply.toolDelay then
-		ply.toolUseTimes = ply.toolUseTimes - 1
-		if ply.toolUseTimes < 0 or ply.curTime > ply.toolDelay + 2 then
-			ply.toolUseTimes = 0
+		ply.curTime = CurTime()
+		ply.toolDelay = ply.toolDelay or 0
+		ply.toolUseTimes = ply.toolUseTimes or 0
+
+		if ply.curTime > ply.toolDelay then
+			ply.toolUseTimes = ply.toolUseTimes - 1
+			diff = ply.curTime - ply.toolDelay
+
+			if ply.toolUseTimes < 0 or diff > 2 then
+				ply.toolUseTimes = 0
+			end
+		else
+			ply.toolUseTimes = ply.toolUseTimes + 1
+			if ply.toolUseTimes > APG.cfg[ "blockToolRate" ].value then
+				return false
+			end
 		end
-	else
-		ply.toolUseTimes = ply.toolUseTimes + 1
-		if ply.toolUseTimes > APG.cfg[ "blockToolRate" ].value then
-			return false
-		end
+
+		ply.toolDelay = ply.curTime + 1
 	end
-
-	ply.toolDelay = ply.curTime + 1
 end)
 
 --[[--------------------
@@ -73,9 +77,10 @@ end)
 ]]----------------------
 
 APG.hookAdd(mod, "CanTool", "APG_ToolWorldControl", function(ply, tr)
-	if not APG.cfg[ "blockToolWorld" ].value then return end
-	if tr.HitWorld and not tr.Entity then
-		return false
+	if APG.cfg[ "blockToolWorld" ].value then
+		if tr.HitWorld and not tr.Entity then
+			return false
+		end
 	end
 end)
 
@@ -84,19 +89,19 @@ end)
 ]]----------------------
 
 APG.hookAdd(mod, "CanTool", "APG_ToolUnfreezeControl", function(ply, tr)
-	if not APG.cfg[ "blockToolUnfreeze" ].value then return end
-	
-	timer.Simple(0.003, function()
-		local ent = tr.Entity
-		local phys = NULL
+	if APG.cfg[ "blockToolUnfreeze" ].value then	
+		timer.Simple(0.003, function()
+			local ent = tr.Entity
+			local phys = NULL
 
-		if IsValid(ent) then
-			phys = ent:GetPhysicsObject()
-			if IsValid(phys) and phys:IsMotionEnabled() then
-				phys:EnableMotion( false )
+			if IsValid(ent) then
+				phys = ent:GetPhysicsObject()
+				if IsValid(phys) and phys:IsMotionEnabled() then
+					phys:EnableMotion( false )
+				end
 			end
-		end
-	end)
+		end)
+	end
 end)
 
 --[[------------------------------------------
